955ae7f87fb77e02d441a252cd5301d63c88243e
Radina Staneva <radistaneva@gmail.com>
2012-06-03 18:22:39 +0300

    iterate over the image with an offset if the qr code is not read successfully the first time

--- src/ZxingReaderTest.groovy
+++ src/ZxingReaderTest.groovy
@@ -6,9 +6,38 @@ import groovy.io.FileType
 import com.google.zxing.*
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource
 import com.google.zxing.common.GlobalHistogramBinarizer
+import com.google.zxing.common.HybridBinarizer
 import com.google.zxing.qrcode.QRCodeReader
 
 class ZxingReaderTest {
+
+	static decode(reader, file, useHints) {
+		def image = ImageIO.read(new File(new String(file)))
+		if (image == null)
+			return
+
+		if (useHints) {
+			for (def x = 0; x < image.width; x+=50) {
+				for (def y = 0; y < image.height; y+=50) {
+					try {
+						def roiWidth = 200, roiHeight = 200
+						if (x + roiWidth > image.width)
+							roiWidth = image.width - x
+						if (y + roiHeight > image.height)
+							roiHeight = image.height - y
+
+						BinaryBitmap binaryMap = new BinaryBitmap(new GlobalHistogramBinarizer(new BufferedImageLuminanceSource(image, x, y, roiWidth, roiHeight)));
+						return reader.decode(binaryMap)
+					} catch (Exception e) {
+					}
+				}
+			}
+		}
+		else {
+			BinaryBitmap binaryMap = new BinaryBitmap(new GlobalHistogramBinarizer(new BufferedImageLuminanceSource(image)))
+			return reader.decode(binaryMap)
+		}
+	}
 
 	static main(args) {
 		def reader = new QRCodeReader()
@@ -17,15 +46,31 @@ class ZxingReaderTest {
 		if (!path.exists())
 			throw new Exception("Directory '$path.name' does not exist")
 
+		def current = 0
+		def total = path.list().length
+		def totalRead = 0
+		def read = false
+		def resultMap = [:]
+
+
 		path.eachFile FileType.FILES, {
 			try {
-				def image = ImageIO.read(new File(it.canonicalPath))
-				def binaryMap = new BinaryBitmap(new GlobalHistogramBinarizer(new BufferedImageLuminanceSource(image)))
-				Result result = reader.decode(binaryMap)
-				println "$it.name $result"
+				Result result = decode(reader, it.canonicalPath, false)
+				resultMap[it.name] = result
+
+				totalRead++
+				read = true
 			} catch (Exception e) {
-				e.printStackTrace()
+				Result result = decode(reader, it.canonicalPath, true)
+
+				resultMap[it.name] = result
+				if (!read) {
+					totalRead++
+					read = false
+				}
 			}
+			println "${++current} / $total : $it.name " + resultMap.get(it.name)
 		}
+		println "Total images read: $totalRead"
 	}
 }
